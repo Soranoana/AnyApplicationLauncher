9009446c880a41060bba98f0451baf7fb6e17542
BzLOVEman <segareyarou@yahoo.co.jp>
2019-12-02 10:32:42 +0900

    いろいろやってる途中

--- Assets/script/mainSystem.cs
+++ Assets/script/mainSystem.cs
@@ -6,22 +6,22 @@ using System.IO;
 public class mainSystem : MonoBehaviour {
 
     private string archivesPath;
-    private int gameCount=0;
+    private int gameCount = 0;
     private RenderTexture rt;
 
     private void Awake() {
-        archivesPath=Application.dataPath+"/../GameArchives/";
+        archivesPath = Application.dataPath + "/../GameArchives/";
         //フォルダ生成
         folderAndFileCreate();
     }
 
-    void Start () {
-		
-	}
-	
-	void Update () {
-		
-	}
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
 
     private void folderAndFileCreate() {
         //アーカイブフォルダが存在しないなら作る
@@ -29,30 +29,30 @@ public class mainSystem : MonoBehaviour {
             DirectoryInfo di = Directory.CreateDirectory(@archivesPath);
         }
         //OXフォルダがないなら作る
-        if (!Directory.Exists(archivesPath+"0X/")) {
-            DirectoryInfo dii = Directory.CreateDirectory(archivesPath+"0X/");
-        }   
-        if (Directory.GetFiles(archivesPath+"0X/", "readme.txt", SearchOption.TopDirectoryOnly).Length==0) {
-            string filePath = archivesPath+"0X/readme.txt";
+        if (!Directory.Exists(archivesPath + "0X/")) {
+            DirectoryInfo dii = Directory.CreateDirectory(archivesPath + "0X/");
+        }
+        if (Directory.GetFiles(archivesPath + "0X/", "readme.txt", SearchOption.TopDirectoryOnly).Length == 0) {
+            string filePath = archivesPath + "0X/readme.txt";
             StreamWriter sw = new StreamWriter(filePath, true);
             sw.WriteLine(readmeData());
             sw.Flush();
             sw.Close();
         }
-        if (Directory.GetFiles(archivesPath+"0X/", "example.exe", SearchOption.TopDirectoryOnly).Length==0) {
-            string filePath = archivesPath+"0X/example.exe";
+        if (Directory.GetFiles(archivesPath + "0X/", "example.exe", SearchOption.TopDirectoryOnly).Length == 0) {
+            string filePath = archivesPath + "0X/example.exe";
             StreamWriter sw = new StreamWriter(filePath, true);
             sw.Flush();
             sw.Close();
         }
-        if (Directory.GetFiles(archivesPath+"0X/", "abstruct.txt", SearchOption.TopDirectoryOnly).Length==0) {
-            string filePath = archivesPath+"0X/abstruct.txt";
+        if (Directory.GetFiles(archivesPath + "0X/", "abstruct.txt", SearchOption.TopDirectoryOnly).Length == 0) {
+            string filePath = archivesPath + "0X/abstruct.txt";
             StreamWriter sw = new StreamWriter(filePath, true);
             sw.Flush();
             sw.Close();
         }
-        if (Directory.GetFiles(archivesPath+"0X/", "movie.ogg", SearchOption.TopDirectoryOnly).Length==0) {
-            string filePath = archivesPath+"0X/movie.ogg";
+        if (Directory.GetFiles(archivesPath + "0X/", "movie.ogg", SearchOption.TopDirectoryOnly).Length == 0) {
+            string filePath = archivesPath + "0X/movie.ogg";
             StreamWriter sw = new StreamWriter(filePath, true);
             sw.Flush();
             sw.Close();
@@ -61,28 +61,28 @@ public class mainSystem : MonoBehaviour {
 
         countGameFolder();
 
-        for (int i = 1; i<=gameCount; i++) {
-            if (!Directory.Exists(Application.dataPath+"/Resources/0"+i.ToString()+"/")) {
+        for (int i = 1; i <= gameCount; i++) {
+            if (!Directory.Exists(Application.dataPath + "/Resources/0" + i.ToString() + "/")) {
                 Debug.Log("ok");
-                DirectoryInfo di = Directory.CreateDirectory(Application.dataPath+"/Resources/0"+i.ToString()+"/");
+                DirectoryInfo di = Directory.CreateDirectory(Application.dataPath + "/Resources/0" + i.ToString() + "/");
                 rt = new RenderTexture(1920, 1080, 24, RenderTextureFormat.ARGB32);
                 Debug.Log(rt.IsCreated());
                 //RenderTextureCreationFlags
                 //  renderTextureCreation
                 //   RenderTextureDescriptor
                 rt.Create();
-                rt.name="RenderTextureVideo";
-                
+                rt.name = "RenderTextureVideo";
+
             }
-            if (i>=9) {
-                for (; i<=gameCount; i++) {
-                    if (!Directory.Exists(Application.dataPath+"/Resources/"+i.ToString()+"/")) {
-                        DirectoryInfo di = Directory.CreateDirectory(Application.dataPath+"/Resources/"+i.ToString()+"/");
+            if (i >= 9) {
+                for (; i <= gameCount; i++) {
+                    if (!Directory.Exists(Application.dataPath + "/Resources/" + i.ToString() + "/")) {
+                        DirectoryInfo di = Directory.CreateDirectory(Application.dataPath + "/Resources/" + i.ToString() + "/");
                         rt = new RenderTexture(1920, 1080, 24, RenderTextureFormat.ARGB32);
                         rt.Create();
-                        rt.name="RenderTextureVideo";
+                        rt.name = "RenderTextureVideo";
                     }
-                    if (i>=99) {
+                    if (i >= 99) {
                         return;
                     }
                 }
@@ -94,12 +94,12 @@ public class mainSystem : MonoBehaviour {
 
     //ゲームフォルダの数を数える
     private void countGameFolder() {
-        for (int i = 1; Directory.Exists(archivesPath+"0"+i.ToString()+"/"); i++) {
+        for (int i = 1; Directory.Exists(archivesPath + "0" + i.ToString() + "/"); i++) {
             gameCount++;
-            if (i>=9) {
-                for (; Directory.Exists(archivesPath+i.ToString()+"/"); i++) {
+            if (i >= 9) {
+                for (; Directory.Exists(archivesPath + i.ToString() + "/"); i++) {
                     gameCount++;
-                    if (i>=99) {
+                    if (i >= 99) {
                         return;
                     }
                 }
